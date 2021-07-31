17795af9528eea5dc11fd43c6ec252a952fece7e
smallwisp <4ubarov96@mail.ru>
2021-07-31 18:13:58 +0300

    completed the tasks of the lesson 2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 2000;
+let income = '200';
+let addExpenses = 'связь, транспорт, развлечения, жилье';
+let deposit = true;
+let mission = 1e6;
+let period = 10;
 
-alert('It\'s time to git!');
-console.log('first lesson');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев.
+Цель заработать ${mission} долларов`);
+
+let arr = addExpenses.toLowerCase().split(',');
+
+console.log(arr);
+
+let budgetDay = money / 30;
+
+console.log(`Дневной доход : ${budgetDay}`);
