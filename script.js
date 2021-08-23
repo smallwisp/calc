3f2bb17c972242c9bffa621cd5b7229697fae89c
smallwisp <46354156+smallwisp@users.noreply.github.com>
2021-08-23 12:29:36 +0300

    completed tasks of lesson 14 part 1

--- script.js
+++ script.js
@@ -9,22 +9,37 @@ function DomElement(selector, height, width, bg, fontSize) {
 
 };
 
-DomElement.prototype.createClassOrId = function(selector) {
+DomElement.prototype.createClassOrId = function(selector, height, width, bg, fontSize) {
+   let anyText = prompt('Введите любой текст:', 'anytext');
+
    if (selector.startsWith('.')) {
-      let getElem = document.createElement('div');
-      document.body.innerHTML = `<div></div>`;
-      getElem = document.querySelector('div');
-      getElem.classList.add(selector);
+      document.body.innerHTML = `<div>` + anyText + `</div>`;
+      let getElem = document.querySelector('div');
+      getElem.classList.add(selector.slice(1))
+      let getElemClass = document.querySelector(`.${selector.slice(1)}`);
+      getElemClass.style.height = height;
+      getElemClass.style.width = width;
+      getElemClass.style.backgroundColor = bg;
+      getElemClass.style.fontSize = fontSize;
    } else if (selector.startsWith('#')) {
-      document.body.innerHTML = `<p></p>"`
+      document.body.innerHTML = `<p>` + anyText + `</p>`;
+      let getElem = document.querySelector('p');
+      getElem.id = selector.slice(1);
+      let getElemId = document.querySelector(`#${selector.slice(1)}`);
+      getElemId.style.height = height;
+      getElemId.style.width = width;
+      getElemId.style.backgroundColor = bg;
+      getElemId.style.fontSize = fontSize;
+
    }
 };
 
-let newObj = new DomElement();
+let newObj = new DomElement('.path1', '10px', '200px', 'red', '16px');
 
-newObj.createClassOrId('.path1', '10px', '200px', 'red', '16px');
+newObj.createClassOrId('#path1', '50px', '200px', 'red', '16px');
 
 console.log(newObj);
+
 
 
 
