80ddec93ab5a520b6ffc3670f301379315ea405d
smallwisp <4ubarov96@mail.ru>
2021-08-23 22:10:04 +0300

    fixed set attributes

--- script.js
+++ script.js
@@ -9,25 +9,32 @@ function DomElement(selector, height, width, bg, fontSize) {
 
 };
 
-DomElement.prototype.createClassOrId = function(selector) {
+DomElement.prototype.createClassOrId = function(selector, height, width, bg, fontSize) {
+   let anyText = prompt('Введите любой текст:', 'anytext');
+   let getElemClassOrId;
    if (selector.startsWith('.')) {
-      let getElem = document.createElement('div');
-      document.body.innerHTML = `<div></div>`;
-      getElem = document.querySelector('div');
-      getElem.classList.add(selector);
+      document.body.innerHTML = `<div>` + anyText + `</div>`;
+      let getElem = document.querySelector('div');
+      getElem.classList.add(selector.slice(1))
+      getElemClassOrId = document.querySelector(`.${selector.slice(1)}`);
+      /* getElemClass.style.height = height;
+      getElemClass.style.width = width;
+      getElemClass.style.backgroundColor = bg;
+      getElemClass.style.fontSize = fontSize; */
    } else if (selector.startsWith('#')) {
-      document.body.innerHTML = `<p></p>"`
+      document.body.innerHTML = `<p>` + anyText + `</p>`;
+      let getElem = document.querySelector('p');
+      getElem.id = selector.slice(1);
+      getElemClassOrId = document.querySelector(`#${selector.slice(1)}`);
    }
+   getElemClassOrId.style.height = height;
+   getElemClassOrId.style.width = width;
+   getElemClassOrId.style.backgroundColor = bg;
+   getElemClassOrId.style.fontSize = fontSize;
 };
 
-let newObj = new DomElement();
+let newObj = new DomElement('.path1', '10px', '200px', 'red', '16px');
 
-newObj.createClassOrId('.path1', '10px', '200px', 'red', '16px');
+newObj.createClassOrId('#path1', '50px', '200px', 'red', '16px');
 
 console.log(newObj);
-
-
-
-
-
-
